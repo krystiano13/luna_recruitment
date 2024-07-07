ebb243c16fc38e922fa0c9466c3da85f60841489
krystiano13 <crystianotv@gmail.com>
2024-07-07 21:14:01 +0200

    create html structure and styles of form used to specify timestamps

--- src/views/Module.tsx
+++ src/views/Module.tsx
@@ -125,6 +125,30 @@ export function Module() {
             </button>
           </div>
         </motion.div>
+        <form className="mt-6 flex flex-col items-start justify-start gap-2">
+          <h2 className="text-white text-xl">Module History:</h2>
+          <div className="flex-col flex items-start justify-start gap-2 mt-6">
+            <label className="text-white">Start:</label>
+            <input type="datetime-local" required name="start" />
+          </div>
+          <div className="flex-col flex items-start justify-start gap-2">
+            <label className="text-white">Stop:</label>
+            <input type="datetime-local" required name="stop" />
+          </div>
+          <div className="flex-col flex items-start justify-start gap-2">
+            <label className="text-white">Mode:</label>
+            <select required>
+              <option>hourly</option>
+              <option>daily</option>
+            </select>
+          </div>
+          <button
+            className="mt-2 hover:bg-emerald-600 hover:border-emerald-600 transition border-2 border-solid bg-emerald-500 border-emerald-500 p-1 pl-4 pr-4 rounded-lg cursor-pointer text-white"
+            type="submit"
+          >
+            Show History
+          </button>
+        </form>
       </div>
     </>
   );
